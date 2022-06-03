acf0b814ad3db6fd53ca8d683f5ae744af6caba9
Abdo2711-coder <abdelrahmandiv@gmail.com>
2022-06-04 01:02:35 +0200

    add scroll to top button

--- src/App.js
+++ src/App.js
@@ -24,6 +24,47 @@ export default function App() {
         })();
     }, []);
 
+    useEffect(() => {
+        const progressPath = document.querySelector(".progress-wrap path");
+        const pathLength = progressPath.getTotalLength();
+        progressPath.style.transition = progressPath.style.WebkitTransition =
+            "none";
+        progressPath.style.strokeDasharray = pathLength + " " + pathLength;
+        progressPath.style.strokeDashoffset = pathLength;
+        progressPath.getBoundingClientRect();
+        progressPath.style.transition = progressPath.style.WebkitTransition =
+            "stroke-dashoffset 10ms linear";
+
+        const updateProgress = function () {
+            const scroll = window.pageYOffset;
+            const height =
+                document.documentElement.offsetHeight - window.innerHeight;
+            const progress = pathLength - (scroll * pathLength) / height;
+            progressPath.style.strokeDashoffset = progress;
+
+            if (scroll > 150) {
+                document
+                    .querySelector(".progress-wrap")
+                    .classList.add("active-progress");
+            } else {
+                document
+                    .querySelector(".progress-wrap")
+                    .classList.remove("active-progress");
+            }
+        };
+        updateProgress();
+        window.addEventListener("scroll", updateProgress);
+
+        return () => {
+            window.removeEventListener("scroll", updateProgress);
+        };
+    }, []);
+
+    const ScrollToTop = (e) => {
+        e.preventDefault();
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    };
+
     return (
         <React.Suspense fallback={<Loader />}>
             <div>
@@ -43,6 +84,21 @@ export default function App() {
                     basic_info={data.basic_info}
                 />
                 <Footer personal_info={data.personal_info} />
+                <div onClick={ScrollToTop} className="progress-wrap">
+                    <span
+                        className="iconify"
+                        data-icon="clarity:angle-line"
+                        data-inline="false"
+                    ></span>
+                    <svg
+                        className="progress-circle"
+                        width="100%"
+                        height="100%"
+                        viewBox="-1 -1 102 102"
+                    >
+                        <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+                    </svg>
+                </div>
             </div>
         </React.Suspense>
     );
